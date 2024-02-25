ae7ca6f8e2e1c7550be0c9e8edda805f35bdd0c5
Uwe Kamper <uk@sys4.de>
2024-02-25 17:52:49 +0100

    add parsing for tlsrpt exts

--- tests/test_tlsrpt_parsing.py
+++ tests/test_tlsrpt_parsing.py
@@ -11,9 +11,26 @@ def test_record_parse_simple_mailto():
     assert parsed.tlsrpt_uri[0] == 'mailto:reports@example.com'
 
 
+def test_record_parse_multiple_mailto():
+    TXT_RECORD="v=TLSRPTv1;rua=mailto:reports@example.com,mailto:postmaster@example.com"
+    parsed = tlsrpt_parsing.record.parseString(TXT_RECORD)
+    assert parsed is not None
+    assert parsed.tlsrpt_version == 'v=TLSRPTv1'
+    assert parsed.tlsrpt_uri[0] == 'mailto:reports@example.com'
+    assert parsed.tlsrpt_uri[1] == 'mailto:postmaster@example.com'
+
+
 def test_record_parse_simple_https():
     TXT_RECORD = "v=TLSRPTv1; rua=https://reporting.example.com/v1/tlsrpt"
     parsed = tlsrpt_parsing.record.parseString(TXT_RECORD)
     assert parsed.tlsrpt_version == 'v=TLSRPTv1'
     assert parsed.tlsrpt_uri[0] == 'https://reporting.example.com/v1/tlsrpt'
+    assert parsed is not None
+
+
+def test_record_parse_with_extension():
+    TXT_RECORD = "v=TLSRPTv1; rua=https://reporting.example.com/v1/tlsrpt; ext=extvalue"
+    parsed = tlsrpt_parsing.record.parseString(TXT_RECORD)
+    assert parsed.tlsrpt_version == 'v=TLSRPTv1'
+    #assert parsed.tlsrpt_uri[0] == 'https://reporting.example.com/v1/tlsrpt'
     assert parsed is not None
--- tlsrpt/tlsrpt_parsing.py
+++ tlsrpt/tlsrpt_parsing.py
@@ -12,47 +12,42 @@ from pyparsing import (
     Literal,
     CaselessLiteral,
     Combine,
-    Group,
-    OneOrMore,
-    Optional,
     ParseException,
-    ParserElement,
     Regex,
-    StringEnd,
     White,
     Word,
     ZeroOrMore,
     alphanums,
-    alphas,
-    nums,
-    printables,
     pyparsing_common,
+    delimitedList,
 )
+
 
 WSP = White(ws=' ', exact=1).suppress()   # Whitespace
 
-field_delim = ZeroOrMore(WSP) + ';' + ZeroOrMore(WSP)   # Fields are semicolon-delimited
-tlsrpt_extension = ZeroOrMore(Literal("TODO"))
+field_delim = ZeroOrMore(WSP) + Literal(';') + ZeroOrMore(WSP)   # Fields are semicolon-delimited
+ura_delim = ZeroOrMore(WSP) + Literal(',') + ZeroOrMore(WSP)   # multiple RUAs are comma-delimited
 
+tlsrpt_ext_name = Word(alphanums, alphanums+"_-.", max=32)
+tlsrpt_ext_value = Word(alphanums, alphanums+"_-.")
+tlsrpt_extension = ZeroOrMore(tlsrpt_ext_name + Literal('=') + tlsrpt_ext_value)
+
+# RegEx for parsing email.
 regex_tld = r"(?:[a-zA-Z]{2,63}|xn--[a-zA-Z0-9]+)"
 regex_mailaddr = (
     r"(?P<mailaddr>([a-zA-Z0-9]{0,61}@)?([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+" r"" + regex_tld + ")"
 )
 mail_uri = Combine(CaselessLiteral("mailto:") + Regex(regex_mailaddr))
+tlsrpt_rua = Literal("rua=") +\
+        delimitedList(mail_uri | pyparsing_common.url, delim=',').setResultsName('tlsrpt_uri')
 
-tlsrpt_rua = Literal("rua=") \
-             + OneOrMore(mail_uri | pyparsing_common.url).setResultsName('tlsrpt_uri')
-
-tlsrpt_field = tlsrpt_rua #  + tlsrpt_extension
-
-#ZeroOrMore(OneOrMore(field_delim) )
-
+tlsrpt_field = tlsrpt_rua + ZeroOrMore(field_delim + tlsrpt_extension)
 
 # Literal will match the version string as required by the ABNF in the RFC:
 # tlsrpt-version    = %s"v=TLSRPTv1"
 version = Literal("v=TLSRPTv1").setResultsName("tlsrpt_version")
 
-record = version + OneOrMore(field_delim + tlsrpt_field)
+record = version + field_delim + tlsrpt_field
 
 
 def parse(tlsrpt_record):
